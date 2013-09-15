ce163e8be121f155787f4f3160c75f26d3a282be
li <gardeniaxy@gmail.com>
2013-09-15 14:55:13 +0800

    完成按班级查看
    Signed-off-by: li <gardeniaxy@gmail.com>

--- web/controllers/status.py
+++ web/controllers/status.py
@@ -36,14 +36,25 @@ class Class(User):
 	def __init__(self):
 		User.__init__(self)
 	def GET(self):
-		plot_data = {'emotion':[], 'data':[]}
-		all_class = db.query('select dinstinct r_class from renren where r_grade in %s'%(str(web.ctx.session.grade_id)))
-		data = db.query('select r_emotion, count(*) from renren where r_grade in %s group by r_emotion order by count(*) ASC'\
-			%(str(web.ctx.session.grade_id)))
-		for emo in data:
-			plot_data['data'].append(emo['count(*)'])
-			plot_data['emotion'].append(emo['r_emotion'].encode('utf-8'))
-		web.ctx.session.emotion = plot_data['emotion']
+		all_emo = ['NONE', 'none', 'happy', 'surprise', 'sad', 'fear', 'angry']
+		all_class = list(db.query('SELECT DISTINCT r_class FROM renren WHERE r_grade IN %s AND r_class <> "NONE"'%(str(web.ctx.session.grade_id))))
+		data = [[] for x in range(len(all_emo))]
+
+		# for each class
+		for c in all_class:
+			if c.r_class == None: continue
+			r = list(db.query("SELECT r_emotion, count(*) AS c FROM renren WHERE r_class = '%s' GROUP BY r_emotion ORDER BY r_emotion ASC"%(c.r_class)))
+			# for each emotion
+			for idx, emo in enumerate(all_emo):
+				tmp = 0
+				for item in r:
+					if item['r_emotion'] == emo:
+						tmp = item['c']
+						break
+				data[idx].append(tmp)
+		plot_data = {'emotion':all_emo, 'data':data,\
+			'class':[c.r_class.encode('utf-8') for c in all_class]}
+		web.ctx.session.emotion = all_emo
 		return render.status.class_(web.ctx.session, plot_data, web.input(start='0', end='0'))
 	def POST(self):
 		pass
